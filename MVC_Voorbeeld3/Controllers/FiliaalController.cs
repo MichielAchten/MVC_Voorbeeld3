5b180120ebbcd1da9154f9326cefb77e1918b71c
MichielAchten <michiel.achten@gmail.com>
2016-09-26 13:46:21 +0200

    CodeHergebruiken1

---
+++ MVC_Voorbeeld3/Controllers/FiliaalController.cs
@@ -0,0 +1,29 @@
+﻿using MVC_Voorbeeld3.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Voorbeeld3.Controllers
+{
+    public class FiliaalController : Controller
+    {
+        // GET: Filiaal
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        private FiliaalService filiaalService = new FiliaalService();
+        private HoofdZetelService hoofdZetelService = new HoofdZetelService();
+
+        public ActionResult Index()
+        {
+            var hoofdZetel = hoofdZetelService.Read();
+            ViewBag.hoofdZetel = hoofdZetel;
+            var filialen = filiaalService.FindAll();
+            return View(filialen);
+        }
+    }
+}
---
+++ MVC_Voorbeeld3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Voorbeeld3.Startup))]
+namespace MVC_Voorbeeld3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
