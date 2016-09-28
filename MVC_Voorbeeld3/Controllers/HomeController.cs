0430b1076379dad70c9a989ac33253c8902c1ce8
MichielAchten <michiel.achten@gmail.com>
2016-09-28 13:40:03 +0200

    GegevensOverMeerderePaginasHeenOnthouden1

--- MVC_Voorbeeld3/Controllers/HomeController.cs
+++ MVC_Voorbeeld3/Controllers/HomeController.cs
@@ -10,7 +10,51 @@ namespace MVC_Voorbeeld3.Controllers
     {
         public ActionResult Index()
         {
+            //sessionvariabele aanpassen
+            this.Session["aantalBezoeken"] = (int)this.Session["aantalBezoeken"] + 1;
+
+            //applicationvariabele aanpassen
+            System.Web.HttpContext.Current.Application.Lock();
+            System.Web.HttpContext.Current.Application["aantalBezoeken"] = (int)System.Web.HttpContext.Current.Application["aantalBezoeken"] + 1;
+            System.Web.HttpContext.Current.Application.UnLock();
+
             return View();
+
+
+            /* Session State */ 
+            //if (this.Session["aantalBezoeken"] == null)
+            //{
+            //    this.Session["aantalBezoeken"] = 1;
+            //}
+            //else
+            //{
+            //    this.Session["aantalBezoeken"] = (int)this.Session["aantalBezoeken"] + 1;
+            //}
+            //return View();
+
+            /* cookie */
+            //string resultaat = "Dit is jouw eerste bezoek";
+
+            ////zijn er cookies?
+            //if (Request.Cookies != null)
+            //{
+            //    //is er een cookie met de naam "user"?
+            //    if (Request.Cookies["lastvisit"] != null)
+            //    {
+            //        //dan lezen we het tijdstip van het laatste bezoek uit de cookie
+            //        resultaat = "Welkom terug. Jouw laatste bezoek was op " + Request.Cookies["lastvisit"]["tijdstip"] + ". Jouw voorkeurtaal is " +
+            //            Request.Cookies["lastvisit"]["taal"] + ".";
+            //    }
+            //    //we slaan het huidige tijdstip op als laatste bezoek
+            //    string laatsteBezoek = DateTime.Now.ToString();
+            //    var userCookie = new HttpCookie("lastvisit");
+            //    userCookie["tijdstip"] = laatsteBezoek;
+            //    userCookie["taal"] = Request.UserLanguages[0];
+            //    userCookie.Expires = DateTime.Now.AddDays(365);
+            //    Response.Cookies.Add(userCookie);
+            //}
+            //ViewBag.Tijdstip = resultaat;
+            //return View();
         }
 
         public ActionResult About()
@@ -25,6 +69,40 @@ namespace MVC_Voorbeeld3.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Wissen()
+        {
+            //reset sessionvariabele
+            this.Session["aantalBezoeken"] = 0;
+
+            //reset applicationvariabele
+            System.Web.HttpContext.Current.Application.Lock();
+            System.Web.HttpContext.Current.Application["aantalBezoeken"] = 0;
+            System.Web.HttpContext.Current.Application.UnLock();
+
+            return View();
+
+            
+            /* Session state */
+            //if (this.Session["aantalBezoeken"] != null)
+            //{
+            //    this.Session["aantalBezoeken"] = null;
+            //}
+            //return View();
+
+            /* cookie */
+            ////zijn er cookies?
+            //if (Request.Cookies != null)
+            //{
+            //    //is er een cookie met de naam "user"
+            //    if (Request.Cookies["lastvisit"] != null)
+            //    {
+            //        Request.Cookies["lastvisit"].Expires = DateTime.Now.AddDays(-1);
+            //        Response.Cookies.Add(Request.Cookies["lastvisit"]);
+            //    } 
+            //}
+            //return View();
         }
     }
 }
